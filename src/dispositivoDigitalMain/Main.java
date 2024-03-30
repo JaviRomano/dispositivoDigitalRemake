dd72520af9fb1742a739601a0f71667e481fcf9b
JaviRomano <159903784+JaviRomano@users.noreply.github.com>
2024-03-30 22:00:49 +0100

    cambio nombre lavadora - prueba
    sin cambios importantes

--- src/dispositivoDigitalMain/Main.java
+++ src/dispositivoDigitalMain/Main.java
@@ -5,7 +5,7 @@ import dispositivoLavadora.Lavadora;
 public class Main {
 		public static void main(String[] args) {
 			
-			Lavadora l1 = new Lavadora(83, (double) 648.99, "Miele", (short) 1650, (long) 25000, (float) 18.5);
+			Lavadora l1 = new Lavadora(83, (double) 648.99, "Balay", (short) 1650, (long) 25000, (float) 18.5);
 			System.out.println(l1.toString());
 			
 		}
